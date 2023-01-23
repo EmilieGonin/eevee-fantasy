7a937a5fe9c31564c6135dc2bdbc623ed2f5c16a
EmilieGonin <emilie.yuu@gmail.com>
2023-01-23 11:04:14 +0100

    add game class

---
+++ Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eevee_fantasy
+{
+    internal class Game
+    {
+        protected int level;
+        public int Level { get; set; }
+        public Game() {
+            //
+        }
+    }
+}
