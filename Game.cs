eb4edeac3e89f0537df7985af266a126b910571e
EmilieGonin <emilie.yuu@gmail.com>
2023-01-24 07:01:00 +0100

    add StreamWriter + StreamReader

--- Game.cs
+++ Game.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace eevee_fantasy
 {
     internal class Game
     {
         protected int level;
-        public int Level { get; set; }
         public Game() {
-            //
+            level = 0;
+
+            using (StreamWriter save = new StreamWriter("save.txt"))
+            {
+                save.WriteLine("test");
+            }
+
+            using (StreamReader save = new StreamReader("save.txt"))
+            {
+                save.ReadLine();
+            }
         }
+        public int Level { get; set; }
     }
 }
