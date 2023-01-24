b545130ebb88b71a95e3626348784d4c9a3caa07
Kelorastus <kelorastus@gmail.com>
2023-01-24 17:53:20 +0100

    Map draw function created and first map created

---
+++ Maps/MapZero.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eevee_fantasy
+{
+    internal class Map
+    {
+        public static void Main()
+        {
+            try
+            {
+                StreamReader mapzero = new StreamReader("levelzero.txt"));
+            
+                string lines;
+
+                while ((lines = mapzero.ReadLine()) != null)
+                {
+                    Console.WriteLine(lines);
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("NOOOOOOO");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+    }
+}
+
+//.. / eevee_fantasy\bin\Debug\Net7 .0\  
