0bd88387a9f8a727ad8b8c6832a59c41b4b2fe82
Kelorastus <kelorastus@gmail.com>
2023-01-24 18:03:52 +0100

    fixed MapZero class

--- Maps/MapZero.cs
+++ Maps/MapZero.cs
@@ -7,34 +7,26 @@ using System.Threading.Tasks;
 
 namespace eevee_fantasy
 {
-    internal class Map
+    internal class MapZero
     {
-        public static void Main()
+        public static void DrawMapZero() //This function should work when called in main
         {
-            try
-            {
-                StreamReader mapzero = new StreamReader("levelzero.txt"));
+        //    StreamReader mapzero = new StreamReader("levelzero.txt")) 
+        //    {           
+        //        string lines;
+
+        //        while ((lines = mapzero.ReadLine()) != null)
+        //        {
+        //            Console.WriteLine(lines);
+        //        }
+
+
+
+        //    }
             
-                string lines;
-
-                while ((lines = mapzero.ReadLine()) != null)
-                {
-                    Console.WriteLine(lines);
-                }
-
-
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("NOOOOOOO");
-                Console.WriteLine(e.Message);
-            }
 
 
         }
 
     }
-}
-
-//.. / eevee_fantasy\bin\Debug\Net7 .0\  
+} 
