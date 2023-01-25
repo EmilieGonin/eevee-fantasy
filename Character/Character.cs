f44c820911b4edd43acfbaa3a0294073685c7e25
EmilieGonin <emilie.yuu@gmail.com>
2023-01-25 09:04:30 +0100

    add character properties

--- Character/Character.cs
+++ Character/Character.cs
@@ -8,8 +8,21 @@ namespace eevee_fantasy
 {
     internal class Character
     {
+
         public string? Name { get; protected set; }
         public Skill[]? Skills { get; }
+
+        public int TotalHp { get; protected set; }
+        public int TotalDef { get; protected set; }
+        public int TotalMagicDef { get; protected set; }
+        public int TotalAtk { get; protected set; }
+        public int BattleHp { get; protected set; }
+        public int Speed { get; protected set; }
+
+        public Character()
+        {
+            TotalHp = TotalDef = TotalAtk = BattleHp = 0;
+        }
 
         public void unlockSkill()
         {
