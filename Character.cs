1a193b66c8b8ba4ecdb91d88fd415bcca336abcf
FreezeKaz <freezekaz.dement@gmail.com>
2023-01-25 08:56:29 +0100

    stats

--- Character.cs
+++ Character.cs
@@ -8,5 +8,31 @@ namespace eevee_fantasy
 {
     internal class Character
     {
+
+        
+	   
+        protected int totalHp, totalDef, totalMagicDef, totalAtk, totalPhysicalBonus, totalMagicalBonus, totalCritRate, totalCritDamage;
+        protected int battleHp;
+
+        protected bool dmgType; // 0 physical, 1 magical
+
+
+        Character() {
+
+            totalHp = 0;
+            totalDef = 0;
+            totalAtk = 0;
+            totalCritRate = 0;
+            totalCritDamage = 0;
+            battleHp = totalHp;
+
+        }
+         ~Character() { }
+
+
+        int Speed (Attr)
+        public int GetBattleHP();
+        public int GetDamageType();
+
     }
 }
