dfd9ab91bea1f3c89b11fa206d60fd723702bad4
FreezeKaz <freezekaz.dement@gmail.com>
2023-01-25 10:26:49 +0100

    battle functions

--- Battle.cs
+++ Battle.cs
@@ -8,5 +8,44 @@ namespace eevee_fantasy
 {
     internal class Battle
     {
+        
+
+        private Character? Character;
+        private Character? Enemy;
+        private int _choiceIndex;
+        
+
+
+        public Battle(Character character, Character enemy)
+        {
+            Character = character;
+            Enemy = enemy;
+            _choiceIndex = 0;
+
+        }
+
+
+        private void MyTurn()
+        {
+            // choice between inventory, attack or party change
+
+            if (_choiceIndex == 0)
+            {
+                //choose skill
+                //attack function
+            }
+        }
+        private void EnnemysTurn()
+        {
+            // AI to determine who's enemy the best to attack 
+        }
+
+        private void Attack(Character attacker, Character target, Skill skillUsed)
+        {
+
+            skillUsed.Use(attacker.Attribute, target); 
+        }
+
+
     }
 }
